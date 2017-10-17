ba59648259e3a2f5675e788a44b25c16ce9d5ffa
xiaopihaier <1428518169@qq.com>
2017-10-17 20:52:11 +0800

    cookbook

--- app/src/main/java/com/example/xiaopihaier/cookbook/Register.java
+++ app/src/main/java/com/example/xiaopihaier/cookbook/Register.java
@@ -4,11 +4,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class Register extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //隐藏标题
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //定义全屏参数
+        int flag = WindowManager.LayoutParams.FLAG_FULLSCREEN;
+        //获得窗口对象
+        Window myWindow = this.getWindow();
+        //设置Flag标识
+        myWindow.setFlags(flag, flag);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
     }
