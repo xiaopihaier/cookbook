167996743865a1d0a16847457ccf922707732974
xiaopihaier <1428518169@qq.com>
2017-10-17 22:26:14 +0800

    cookbook

--- app/src/main/java/com/example/xiaopihaier/cookbook/Forget_password.java
+++ app/src/main/java/com/example/xiaopihaier/cookbook/Forget_password.java
@@ -7,11 +7,16 @@ import android.view.KeyEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class Forget_password extends AppCompatActivity implements View.OnClickListener {
 
     ImageView black;
+    EditText username_input,password_input,Confirmation_password;
+    Button Confirmation_modification;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +34,11 @@ public class Forget_password extends AppCompatActivity implements View.OnClickLi
     }
 
     private void InitView() {
+        username_input= (EditText) findViewById(R.id.username_input);
+        password_input= (EditText) findViewById(R.id.password_input);
+        Confirmation_password= (EditText) findViewById(R.id.Confirmation_password);
+        Confirmation_modification= (Button) findViewById(R.id.Confirmation_modification);
+        Confirmation_modification.setOnClickListener(this);
         black = (ImageView) findViewById(R.id.black);
         black.setOnClickListener(this);
     }
@@ -51,6 +61,10 @@ public class Forget_password extends AppCompatActivity implements View.OnClickLi
                 Intent login = new Intent(Forget_password.this, Login.class);
                 startActivity(login);
                 this.finish();
+                break;
+            case R.id.Confirmation_modification:
+                username_input.getText().toString();
+                Toast.makeText(Forget_password.this,"修改成功",Toast.LENGTH_LONG).show();
                 break;
         }
     }
